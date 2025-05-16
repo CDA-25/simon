dc851ed11d5063f926050aa709dd208495673dba
NicolasBondenet <36343149+Vaquicraft@users.noreply.github.com>
2025-05-16 15:56:34 +0200

    create randomColor #22

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function getRandomColor() {
+    let numberColor = Math.trunc((Math.random() * 4 ) + 1)
+    switch (numberColor) {
+        case (1): {
+            return "red"
+        } case (2): {
+            return "green"
+        } case (3): {
+            return "blue"
+        } case (4): {
+            return "yellow"
+        }
+    }
+}
+
