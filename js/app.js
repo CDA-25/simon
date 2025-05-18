449e21e16a437bd551990d2c4582fe8100e58e64
Ryad.253 <ryad.elm@icloud.com>
2025-05-18 19:26:28 +0200

    work in progress

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// window.addEventListener("load", function() {
+
+// // aller chercher les éléménts dans le html
+
+// // function cliquer pour jouer
+// btnPlay.addEventListener("clique", function() {})
+
+
+//const caseSelector = document.querySelector(".container")
+// })
+
+const colors = ['red', 'blue', 'green', 'yellow'] // Je crée mon tab de couleur
+
+let iaColorsSerie = []
+let userColorsSerie = []
+
+function getRandomColor(colors) {                       // Fonction pour avoir une couleur aléatoire
+    const index = Math.round(Math.random() * 3)      // declare une const qui récupere un chiffe aléatoire de 0 à 3
+    const color = colors[index]                       // declare une const qui est égale a l'index aleatoire du tableau colors
+    return color   
+}
+
+function pushColortoIASerie(color) {
+    iaColorsSerie.push(color)
+}
+
+
+console.log(getRandomColor(colors))
+
+let color = getRandomColor(colors)
+
+pushColortoIASerie(color)
+
+console.log(iaColorsSerie)
+
+
+// il faut que je crée un timer de 5 seconde qui se remet à 0 entre chaque clique et if timer = 0, partie perdu
