251ecc0e46f444b03f01261f86077cce183e42df
Ryad.253 <ryad.elm@icloud.com>
2025-05-22 09:27:04 +0200

    ryad

--- js/app.js
+++ js/app.js
@@ -35,3 +35,40 @@ console.log(iaColorsSerie)
 
 
 // il faut que je crée un timer de 5 seconde qui se remet à 0 entre chaque clique et if timer = 0, partie perdu
+
+window.addEventListener("load", function () {
+
+    // aller chercher les éléménts dans le html
+
+    const redContainer = document.getElementsByClassName("red")
+    const greenContainer = document.getElementsByClassName("green")
+    const blueContainer = document.getElementsByClassName("blue")
+    const yellowContainer = document.getElementsByClassName("yellow")
+    const btnPlay = document.querySelector(".btnJouer")
+    const allContainers = document.querySelectorAll(".container")
+
+    // function cliquer pour jouer
+    btnPlay.addEventListener("click", () => {
+        lockClick()
+
+    })
+
+
+    //functions pour bloquer ou débloquer les cliques ici >>>
+    let clickLocked = false
+
+    function lockClick() {
+        clickLocked = true
+        allContainers.forEach(container => {
+            container.classList.add("no-click")
+        })
+    }
+
+    function unlockClick() {
+        clickLocked = false
+        allContainers.forEach(container => {
+            container.classList.remove("no-click")
+        })
+    }
+
+})
