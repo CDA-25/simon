85cfbd39f0c5a2bfe2496d1ac9f7c53e81aba176
NicolasBondenet <36343149+Vaquicraft@users.noreply.github.com>
2025-05-22 10:27:31 +0200

    add functions randomNumber && numberToColor

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
-function getRandomColor() {
-    let numberColor = Math.trunc((Math.random() * 4 ) + 1)
-    switch (numberColor) {
-        case (1): {
-            return "red"
-        } case (2): {
-            return "green"
-        } case (3): {
-            return "blue"
-        } case (4): {
-            return "yellow"
-        }
-    }
+function getRandomNumber() {
+    return Math.trunc((Math.random() * 4) + 1)
 }
 
+function numberToColor(input) {
+    switch (input) {
+        case 1: return "red"
+        case 2: return "green"
+        case 3: return "blue"
+        case 4: return "yellow"
+        default: return null
+    }
+}
