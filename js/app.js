93c7fa97b986aad442213307b9a7bb4581d28b7e
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-05-22 11:43:43 +0200

    js fini

--- js/app.js
+++ js/app.js
@@ -10,16 +10,40 @@ const red = document.getElementById("red");
 const yellow = document.getElementById("yellow");
 const blue = document.getElementById("blue");
 
+green.addEventListener("click", () => playerSequence("green"));
+red.addEventListener("click", () => playerSequence("red"));
+yellow.addEventListener("click", () => playerSequence("yellow"));
+blue.addEventListener("click", () => playerSequence("blue"));
+
 function couleurAleatoire() {
   return colors[Math.floor(Math.random() * colors.length)];
 }
-console.log(couleurAleatoire());
+//console.log(couleurAleatoire());
 
 function simonSequence() {
   simonPlay.push(couleurAleatoire());
   return simonPlay;
 }
-console.log(simonSequence());
+//console.log(simonSequence());
+
+function playSimonSequence() {
+  let i = 0;
+  const interval = setInterval(() => {
+    buttonActive(simonPlay[i]);
+    i++;
+    if (i >= simonPlay.length) {
+      clearInterval(interval);
+      canClick = true;
+    }
+  }, 1000);
+}
+
+const buttons = {
+  green: green,
+  red: red,
+  yellow: yellow,
+  blue: blue,
+};
 
 function buttonActive(color) {
   buttons[color].classList.add("active");
@@ -29,6 +53,53 @@ function buttonActive(color) {
   }, 500);
 }
 
-function playerSequence() {}
+function playerSequence(color) {
+  if (!canClick) return;
 
-function startGame() {}
+  playerPlay.push(color);
+  buttonActive(color);
+
+  const indexActuelle = playerPlay.length - 1;
+  if (playerPlay[indexActuelle] !== simonPlay[indexActuelle]) {
+    gameOver = true;
+    canClick = false;
+    document.getElementById("score").textContent =
+      "Game Over! ton score est de :" + score;
+    return;
+  }
+
+  if (playerPlay.length === simonPlay.length) {
+    score++;
+    updateScore();
+    setTimeout(nextRound, 1000);
+  }
+}
+
+function nextRound() {
+  playerPlay = [];
+  canClick = false;
+  const couleurSuivante = couleurAleatoire();
+  simonPlay.push(couleurSuivante);
+  playSimonSequence();
+}
+function updateScore() {
+  document.getElementById("score").textContent = "Score: " + score;
+}
+
+document.getElementById("start").addEventListener("click", () => {
+  if (gameOver) {
+    startGame();
+  } else {
+    nextRound();
+  }
+});
+
+function startGame() {
+  playerPlay = [];
+  simonPlay = [];
+  score = 0;
+  gameOver = false;
+  canClick = false;
+  updateScore();
+  nextRound();
+}
