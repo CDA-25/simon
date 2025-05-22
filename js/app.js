99aa4f05df06b73c8d3a9972c217546a4ff1602c
Pablo Garcia <pablogarousseau@gmail.com>
2025-05-22 11:46:59 +0200

    voula walou

--- js/app.js
+++ js/app.js
@@ -1,32 +1,98 @@
 const btnContainer = document.getElementsByClassName("btnContainer")
 
 const newBtn = document.getElementsByClassName("btn")
+const startBtn = document.getElementById("start")
+const score = document.getElementById("score")
+const end = document.getElementById("bravo")
 
 const colorRed = document.getElementById("red")
 const colorBlue = document.getElementById("blue")
 const colorGreen = document.getElementById("green")
 const colorYellow = document.getElementById("yellow")
 
-let playerAllowed = false
+let counter = 1
+let sequence = []
+let userSequence = []
+let userAllowed = false
 
 colorRed.addEventListener("click", () => {
-    if (playerAllowed === true) {
+    if (userAllowed) {
         changeColor(colorRed)
+        userSequence.push(1)
+        if (userSequence.length === sequence.length) {
+            if (JSON.stringify(userSequence) === JSON.stringify(sequence)) {
+                counter++
+                userAllowed = false
+                userSequence = []
+                score.innerHTML = "Score :<br>" + (counter - 1)
+                setTimeout(() => {
+                    game(counter)
+                }, 1000);
+            } else {
+                end.textContent = "Bravo !"
+                resetGame()
+            }
+        }
     }
 })
 colorBlue.addEventListener("click", () => {
-    if (playerAllowed === true) {
+    if (userAllowed) {
         changeColor(colorBlue)
+        userSequence.push(2)
+        if (userSequence.length === sequence.length) {
+            if (JSON.stringify(userSequence) === JSON.stringify(sequence)) {
+                counter++
+                userAllowed = false
+                userSequence = []
+                score.innerHTML = "Score :<br>" + (counter - 1)
+                setTimeout(() => {
+                    game(counter)
+                }, 1000);
+            } else {
+                end.textContent = "Bravo !"
+                resetGame()
+            }
+        }
     }
 })
 colorGreen.addEventListener("click", () => {
-    if (playerAllowed === true) {
+    if (userAllowed) {
         changeColor(colorGreen)
+        userSequence.push(3)
+        if (userSequence.length === sequence.length) {
+            if (JSON.stringify(userSequence) === JSON.stringify(sequence)) {
+                counter++
+                userAllowed = false
+                userSequence = []
+                score.innerHTML = "Score :<br>" + (counter - 1)
+                setTimeout(() => {
+                    game(counter)
+                }, 1000);
+            } else {
+                end.textContent = "Bravo !"
+                resetGame()
+            }
+        }
     }
 })
 colorYellow.addEventListener("click", () => {
-    if (playerAllowed === true) {
+    if (userAllowed) {
         changeColor(colorYellow)
+        userSequence.push(4)
+        if (userSequence.length === sequence.length) {
+            if (JSON.stringify(userSequence) === JSON.stringify(sequence)) {
+                counter++
+                userAllowed = false
+                userSequence = []
+                score.innerHTML = "Score :<br>" + (counter - 1)
+                setTimeout(() => {
+                    game(counter)
+                }, 1000);
+            } else {
+                end.textContent = "Bravo !"
+                resetGame()
+            }
+        }
     }
 })
 
@@ -36,53 +102,47 @@ function changeColor(color) {
     setTimeout(() => {
         color.style.opacity = 0.25
         color.style.transition = "opacity 0.35s"
-    }, 750);
+    }, 500);
 }
 
 function randomNumberBetween1and4() {
     return Math.floor(Math.random() * 4) + 1
 }
 
-
-function onOffButton(nb) {
-    return new Promise((resolve) => {
-        const colorSuit = []
-
-        for (let i = 0; i < nb; i++) {
-            setTimeout(() => {
-                const colorNb = randomNumberBetween1and4()
-                if (colorNb === 1) {
-                    changeColor(colorRed)
-                    colorSuit.push("red")
-                }
-                else if (colorNb === 2) {
-                    changeColor(colorBlue)
-                    colorSuit.push("blue")
-                }
-                else if (colorNb === 3) {
-                    changeColor(colorGreen)
-                    colorSuit.push("green")
-                }
-                else if (colorNb === 4) {
-                    changeColor(colorYellow)
-                    colorSuit.push("yellow")
-                }
-
-                if (i === nb - 1) {
-                    playerAllowed = true
-                    resolve(colorSuit)
-                }
-            }, i * 1000)
-        }
-    })
+async function onOffButton(nb) {
+    let randomNumber = randomNumberBetween1and4()
+    sequence.push(randomNumber)
+    for (let i = 0; i < sequence.length; i++) {
+        await new Promise(resolve => setTimeout(resolve, 750))
+        if (sequence[i] === 1) changeColor(colorRed)
+        if (sequence[i] === 2) changeColor(colorBlue)
+        if (sequence[i] === 3) changeColor(colorGreen)
+        if (sequence[i] === 4) changeColor(colorYellow)
+    }
 }
-
-let counter = 1
 
 async function game(nb) {
-    let suitTab = await onOffButton(nb)
-    console.log(suitTab)
+    end.textContent = ""
+    await onOffButton(nb)
+    console.log(sequence)
+    userAllowed = true
 }
 
-
-game(counter)
+function resetGame() {
+    sequence = []
+    userSequence = []
+    counter = 1
+    userAllowed = false
+}
+    
+startBtn.addEventListener("click", () => {
+    setTimeout(() => {
+        changeColor(colorRed)
+        changeColor(colorBlue)
+        changeColor(colorGreen)
+        changeColor(colorYellow)
+        setTimeout(() => {
+            game(counter)
+        }, 1000);
+    }, 250);
+})
